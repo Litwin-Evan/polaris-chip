bf3c68cd59e734116e205d75eb2eacd1567475a0
Evan Litwin <eflitwin2004@gmail.com>
2026-02-13 10:20:01 -0500

    Update my-card component styles and functionality; enhance card details and layout

--- src/my-card.js
+++ src/my-card.js
@@ -18,24 +18,34 @@ export class MyCard extends LitElement {
     this.title = "My card";
     this.image = "https://cdn.britannica.com/36/162636-050-932C5D49/Colosseum-Rome-Italy.jpg";
     this.alt = "Rome Colosseum";
+    this.detailsLabel = "Details";
+    this.fancy = false;
+    this.link = "https://hax.psu.edu";
   }
 
-  static get styles() {
-    return css`
-    body {
-  background: white;
-  padding: 36px;
-} 
+static get styles() {
+  return css`
+  
+:host([fancy]) .card {
+      background: linear-gradient(135deg, #6b83d8, #ec6666);
+      border: solid #ff0000 2px;
+      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
+    }
 
 .card {
   background: white;
   width: 550px;
+  height: 600px;
   border: solid black 2px;
   border-radius: 25px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
   display: inline-block;
   margin-top: 25px;
   text-align: center;
+  overflow-y: scroll;
+  scrollbar-width: thin;
+
+
 }
 
 .card-image {
@@ -46,7 +56,8 @@ export class MyCard extends LitElement {
 
 .card-image img {
   width: 100%;
-  height: auto;
+  height: 350px;
+  object-fit: cover;
   display: block;
 }
 
@@ -75,6 +86,24 @@ export class MyCard extends LitElement {
   color: white;
 }
 
+ details summary {
+    text-align: left;
+    font-size: 20px;
+    padding: 8px 0;
+  }
+
+  details[open] summary {
+    font-weight: bold;
+  }
+  
+  details div {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    height: 70px;
+    overflow: auto;
+  }
+
 @media (min-width: 500px) and (max-width: 800px) {
   .card .button {
     display: inline-block;
@@ -91,32 +120,60 @@ export class MyCard extends LitElement {
 }
  
       :host {
-        display: block;
+        display: inline-block;
         margin: 16px;
         font-family: "Times New Roman", serif;
       }
-    `;
-  }
+  `;
+}
 
-  render() {
-    return html` <div class="card">
-    <img class='card-image' src="${this.image}" alt="${this.title}" />
-    <div class='card-text'>
+openChanged(e) {
+  console.log(e);
+  if (e.target.getAttribute('open') !== null) {
+    this.fancy = true;
+  }
+  else {
+    this.fancy = false;
+  }
+}
+
+render() {
+    return html` 
+    <div class="card">
+
+      <div img class='card-image'>
+        <img src="${this.image}" alt="${this.title}">
+      </div>
+
+      <div class='card-text'>
       <h2 class="card-title">${this.title}</h2>
+      
       <div class="details">
-        <slot></slot>
-       </div>
-      <button class="button">Details</button>
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+          <summary>Description</summary>
+          <div>
+            <slot>${this.description}</slot>
+          </div>
+        </details>
+      </div>
+
+      <a href="${this.link}">
+        <button class="button">
+          <slot name="button">${this.detailsLabel}</slot>
+        </button>
+      </a>
     </div>
-  </div>`;    
+    </div>`;    
   }
 
   static get properties() {
     return {
       title: { type: String },
       image: { type: String },
-      alt: { type: String }
-
+      alt: { type: String },
+      detailsLabel: { type: String },
+      fancy: { type: Boolean, reflect: true },
+      link: { type: String }
     };
   }
 }
